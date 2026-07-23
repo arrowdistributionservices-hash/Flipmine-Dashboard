4a130cb102fcbdc0cb1df879de4ffe46bfe4970e
Your Name <you@example.com>
2026-07-24 03:56:57 +0500

    Add debug diagnostics for daily purchasing block detection

--- pages/api/data.js
+++ pages/api/data.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    purchasingSummary = await fetchDailyPurchasingSummary();
+    purchasingSummary = await fetchDailyPurchasingSummary(wantDebug);
   } catch (e) {
     // leave purchasingSummary at empty default
   }
